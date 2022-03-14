c0be432a35d94daa6aec1beac9893c742f453a7a
Guada Fermani <guadalupefermani@gmail.com>
2022-03-14 18:07:00 -0300

    Solución Reto #9

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge9.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge9.kt
@@ -20,3 +20,69 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
+fun main(){
+    val input = "Charquito? mastica.."
+    val morseInput = decoder(input)
+    println("De natural a morse la frase: \"$input\"")
+    println(decoder(input))
+    println("De morse a natural la frase: \"$morseInput\"")
+    println(decoder(morseInput))
+}
+
+fun isMorse(input: String): Boolean {
+    return input.all { it == '.' || it == '—' || it == ' '}
+}
+
+private fun decoder(input: String): String {
+
+    var decoded = ""
+
+    val naturalMap = mapOf("A" to ".—", "N" to "—.", "0" to "—————",
+        "B" to "—...", "Ñ" to "——.——", "1" to ".————",
+        "C" to "—.—.", "O" to "———", "2" to "..———",
+        "CH" to "————", "P" to ".——.", "3" to "...——",
+        "D" to "—..", "Q" to "——.—", "4" to "....—",
+        "E" to ".", "R" to ".—.", "5" to ".....",
+        "F" to "..—.", "S" to "...", "6" to "—....",
+        "G" to "——.", "T" to "—", "7" to "——...",
+        "H" to "....", "U" to "..—", "8" to "———..",
+        "I" to "..", "V" to "...—", "9" to "————.",
+        "J" to ".———", "W" to ".——", "." to ".—.—.—",
+        "K" to "—.—", "X" to "—..—", "," to "——..——",
+        "L" to ".—..", "Y" to "—.——", "?" to "..——..",
+        "M" to "——", "Z" to "——..", "\"" to ".—..—.", "/" to "—..—.")
+
+    val morseMap = mutableMapOf<String, String>()
+    naturalMap.forEach {
+        morseMap[it.value] = it.key
+    }
+
+    if (isMorse(input)) {
+        input.split("  ").forEach { word ->
+             word.split(" ").forEach { symbols ->
+                 if (symbols.isNotEmpty()) {
+                     decoded += morseMap[symbols]
+                 }
+            }
+            decoded += " "
+        }
+
+    } else {
+        val processedInput = input.uppercase()
+        var index = 0
+        while (index < processedInput.length) {
+            if (processedInput[index] == 'C' && index + 1 < processedInput.length && processedInput[index+1] == 'H'){
+                decoded += naturalMap["CH"]
+                index++
+            } else if (processedInput[index] == ' ') {
+                decoded += " "
+            } else {
+                decoded += naturalMap[processedInput[index].toString()]
+            }
+            decoded += " "
+            index++
+        }
+    }
+
+    return decoded
+}
